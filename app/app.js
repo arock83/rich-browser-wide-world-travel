45abc05eba1eea0eb26055784438ddaf92be100c
Andrew Rock <Andrew Rock>
2017-06-01 11:35:26 -0500

    added routing

--- app/app.js
+++ app/app.js
@@ -1,4 +1,12 @@
 "use strict";
 
-var app = angular.module("TravelApp", []);
+var app = angular.module("TravelApp", ["ngRoute"]);
 
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'partials/guide-list.html',
+		controller: 'bookCtrl'
+	})
+	.otherwise('/');
+});
